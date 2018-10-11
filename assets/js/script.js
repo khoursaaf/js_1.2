3ebdbe1050cd09ece48654299dcb35f1e91b2b2b
khoursaaf <khoursa@gmail.com>
2018-10-11 10:24:20 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// alert('Nom : Doe\nPrenom : John\nVille : New York');
+
+var firstName = "Doe";
+var lastName = "John";
+var city = "New York";
+  alert(
+   'Nom : ' + firstName + '\n' + 'Prenom : ' + lastName + '\n' + 'Ville : ' + city
+  );
